2d62b74b1100790c279c10aa7e3ab48da5f38966
PolinaBrusova <13polbr@gmail.com>
2022-03-21 18:44:07 +0300

    Документирование
    Добавлено документирование класса и методов

--- TourGuide.py
+++ TourGuide.py
@@ -2,9 +2,29 @@ import pandas as pd
 
 
 class TourGuide(object):
-    """Класс организатора путешествий"""
+    """Класс организатора путешествий
+    Класс организует составление маршрута по предоставленным данным,
+    учитывая важность и время на посещение мест.
+
+    Attributes
+    ----------
+    self.data: pandas.DataFrame
+        данные для составления маршрута
+    self.data_storage: pandas.DataFrame
+        копия данных для последующего востановления
+    self.overall_time: int
+        общее время для составления маршрута в часах
+    self.sleep_time: int
+        время на сон в часах
+    self.day_time: int
+        время дня в часах (фиксированные 24 часа)
+    self.path: dictionary
+        словарное представление маршрута
+    """
 
     def __init__(self, data, overall_time=48, sleep_time=8):
+        """Конструктор класса"""
+
         self.data = data.sort_values('Важность', ascending=False, ignore_index=True)
         self.data_storage = self.data
         self.overall_time = overall_time
@@ -13,6 +33,12 @@ class TourGuide(object):
         self.path = {}
 
     def __find_steps(self, remaining_time):
+        """
+        Рекурсивно получает список мест для посещения с учетом оставшегося в дне времени
+        :param remaining_time: оставшееся время в часах
+        :return: список pd.Series с подобранными элементами
+        """
+
         res = self.data[self.data["Затраты времени на посещение"] == remaining_time].reset_index(drop=True)
         if len(res) > 0:
             return res.loc[[0]]
@@ -27,6 +53,12 @@ class TourGuide(object):
                 return None
 
     def __flatten(self, l1):
+        """
+        Рекурсивно приводит списка из N-D к 1-D
+        :param l1: список, которые надо привести к 1-D
+        :return: список 1-D
+        """
+
         if len(l1) == 1:
             if type(l1[0]) == list:
                 result = self.__flatten(l1[0])
@@ -39,6 +71,11 @@ class TourGuide(object):
         return result
 
     def __pretty_print(self):
+        """
+        Выводит информацию о построенном расписании в понятном виде
+        :return: None
+        """
+
         for k, v in self.path.items():
             time_passed = 0
             print(f"Расписание {k} дня:")
@@ -55,10 +92,20 @@ class TourGuide(object):
                 print(v + "\n")
 
     def __restore(self):
+        """
+        Восстанавливает измененную дату и обновляет словарное представление маршрута
+        :return: None
+        """
+
         self.data = self.data_storage
         self.path = {}
 
     def build_route(self):
+        """
+        Метод организации построения маршрута, вычисляющий маршрут для каждого дня
+        :return: None
+        """
+
         self.__restore()
         for day in range(self.overall_time // self.day_time):
             self.path[day + 1] = {"Сон": self.sleep_time}
@@ -76,11 +123,29 @@ class TourGuide(object):
         self.__pretty_print()
 
     def set_data(self, new_data):
+        """
+        Устанавливает новое значение для self.data
+        :param new_data: pd.DataFrame
+        :return: None
+        """
+
         self.data = new_data.sort_values('Важность', ascending=False, ignore_index=True)
         self.data_storage = self.data
 
     def set_overall_time(self, new_time):
+        """
+        Устанавливает новое значение для self.overall_time
+        :param new_time: int
+        :return: None
+        """
+
         self.overall_time = new_time
 
     def set_sleep_time(self, new_time):
+        """
+        Устанавливает новое значение для self.sleep_time
+        :param new_time: int
+        :return: None
+        """
+
         self.sleep_time = new_time
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 if __name__ == "__main__":
     data = pd.read_csv('data.txt', sep=";", header=0, decimal=',')
+    
     ourPathGuide = TourGuide(data)
     ourPathGuide.build_route()
     ourPathGuide.set_overall_time(24)
